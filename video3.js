b6b2cf89217b19daf1d43feccf972c678fe266a6
ganeshapmb <63532509+ganeshapmb@users.noreply.github.com>
2020-04-14 15:24:15 +0530

    video3 of vue
    video3 of vue

---
+++ video3.js
@@ -0,0 +1,15 @@
+var myview = new Vue({
+    el: "#our-app",
+    data: {
+        message: "Hi, Everyone",
+        fname: "Banda",
+        url: "https://www.facebook.com/",
+        title:"WhatsUp by Facebook",
+        image: "IMG_20191220_200753.jpg",
+        isActive:true,
+        txtName:"Banda",
+        txtEmail:"banda@news24.co",
+        htmlContext:"<p>My group</p>"
+    }
+
+});
