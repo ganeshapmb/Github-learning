35f6e818d34c076a7cbd4f3548c388e32bcc1d58
ganeshapmb <63532509+ganeshapmb@users.noreply.github.com>
2020-04-16 12:02:38 +0530

    video8 of vue
    video8 of vue

---
+++ video8.js
@@ -0,0 +1,11 @@
+var myview = new Vue({
+    el: "#our-app",
+    data: {
+       
+    },
+    methods:{
+        keyupfunction: function () {
+          console.log("I am watching ...");  
+        }
+    }
+});
