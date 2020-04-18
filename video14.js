d31eb4d19dd6edad77654cbf993231ccf72edbaa
ganeshapmb <63532509+ganeshapmb@users.noreply.github.com>
2020-04-18 20:42:21 +0530

    video14 of vue
    video14 of vue

---
+++ video14.js
@@ -0,0 +1,13 @@
+var myview = new Vue({
+    el: "#our-app",
+    data: {
+        items:["Oats","Quinoa","WheatBran","BrownRice"],
+        facultys:[
+            {name:"Mounika", subject:"VISUAL CODE" ,qualification:{school:"VSEC",college:"chapra university"}},
+            {name:"Maya", subject:"Math" ,qualification:{school:"DPS",college:" KPS"}}
+        ]        
+    },
+    methods:{
+    }
+
+});
